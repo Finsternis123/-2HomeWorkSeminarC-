8221eaea662a4b10b4a18c69c2b4781c22464bba
Vladislav <zomba4o4ek100@gmail.com>
2022-12-21 21:02:50 +0300

    First task complete

---
+++ Task№1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Обязательна проверка на ввод чисел больше/меньше 3-х знаков
+Console.Clear();
+Console.WriteLine("Enter number ");
+int num = int.Parse(Console.ReadLine()!);
+
+int a = 1000;
+int b = 100;
+int c = 10;
+int result = 0;
+
+if (num < b)
+{
+     Console.WriteLine($"Error");
+}
+else
+{
+    if (num < a)
+{
+    result = num / c;
+    Console.WriteLine($"Second number {(result % c)}");
+}
+else
+{
+    Console.WriteLine($"Error");
+}
+}
+
+
+
+   
