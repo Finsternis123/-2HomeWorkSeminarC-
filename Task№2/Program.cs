6316d27224d8739a272d62c0d00d1ef5e99f604d
Vladislav <zomba4o4ek100@gmail.com>
2022-12-21 22:45:46 +0300

    Second task complete

---
+++ Task№2/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Берем числа до 100000
+Console.Clear();
+Console.WriteLine("Enter number ");
+int num = int.Parse(Console.ReadLine()!);
+
+int a = 100000;
+int b = 10000;
+int c = 1000;
+int d = 100;
+int e = 10;
+int result = 0;
+
+    if (num < d)
+{
+     Console.WriteLine($"Error");
+}
+else
+    if (num <= c)
+{
+    result = num;
+    Console.WriteLine($"Second number {(result % e)}");
+}
+else
+    if (num <= b)
+{
+    result = num / e;
+    Console.WriteLine($"Second number {(result % e)}");
+}
+else
+    if (num <= a)
+{
+    result = num / d;
+    Console.WriteLine($"Second number {(result % e)}");
+}
+
+
+
+
+
